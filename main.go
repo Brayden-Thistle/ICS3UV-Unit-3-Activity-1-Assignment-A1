5402bcdedb385fc2580a9f06b7ee932bc1555c67
Brayden Thistle <bthistle@huronperthcatholic.ca>
2025-11-13 15:29:25 +0000

    added a Go and TS program that displays the area of a circle

---
+++ main.go
@@ -0,0 +1,20 @@
+/*
+* @author Brayden Thistle
+* @version 1.0.0
+* @date 2025-11-13
+* @fileoverfiew this program displays the area of a circle
+*/
+
+package main
+
+import "fmt"
+
+func main () {
+	//creating a constant for pi
+	const pi float32 = 3.14159
+
+//displaying the answer
+	fmt.Println("The area of a circle with the radius of 5.6cm is ", (5.6 * 5.6 * pi), "cm²")
+
+	fmt.Println("\nDone")
+}
---
+++ main.ts
@@ -0,0 +1,14 @@
+/**
+ * @author Brayden Thistle
+ * @version 1.0.0
+ * @date 2025-11-13
+ * @fileoverview this program displays the area of a circle
+ */
+
+//create the constant
+let pi: number = 3.14159;
+
+//display the answer
+console.log("the area of a circle with the radius of 5.6cm is " + (5.6 * 5.6 * pi) + "cm²");
+
+console.log("\nDone");
